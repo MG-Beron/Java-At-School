fee5cca39a886424413f4d5c06a21d0dfcd29df0
Denev <stelian.denev@gmail.com>
2015-10-18 00:00:49 +0300

    A task

---
+++ TwoSameDigits500To600/src/TwoSameDigits500To600.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Denev on 17.10.2015 ã..
+ */
+public class TwoSameDigits500To600 {
+    public static void main(String[] args) {
+
+        int count = 0;
+        for (int i = 500; i <= 600; i++) {
+
+            int ones = i % 10;
+            int tens = i / 10 % 10;
+            int hundreds = i / 100 % 10;
+
+            if (ones == tens || ones == hundreds || tens == hundreds){
+                count++;
+                System.out.printf("\nNumber %d -> %d", count, i);
+            }
+        }
+    }
+}
