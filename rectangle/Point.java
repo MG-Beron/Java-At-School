f69f70a8cac45cde95d1f6ae834bbd20eb8ce2f6
Dako Dimov <dako98@gmail.com>
2016-10-25 18:25:00 +0300

    Update Point.java

--- rectangle/Point.java
+++ rectangle/Point.java
@@ -1,36 +1,41 @@
 package rectangle;
 
 public class Point {
-
+    
     private int x;
     private int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    
+    
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
     }
-
-    public Point() {
-        this(0, 0);
+    
+    public Point(){
+        this(0,0);
     }
-
-    public int getX() {
+    
+    public int getX(){
         return this.x;
     }
-
-    public int getY() {
+    
+    public int getY(){
         return this.y;
     }
 
-    public double calculateDistance(Point point) {
-        double deltaX = this.x - point.getX();
-        double deltaY = this.y - point.getY();
-
-        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    public double distance(Point a){
+        if(this.x==a.getX() || this.y==a.getY()){
+            throw new IllegalArgumentException("Identicle points.");
+        }
+        else{
+        double deltaX = this.x-a.getX();
+        double deltaY = this.y-a.getY();
+        return Math.sqrt(deltaX*deltaX+deltaY*deltaY);
+        }
     }
-
+    
     @Override
-    public String toString() {
-        return String.format("x=%d;y=%d", this.getX(), this.getY());
+    public String toString(){
+        return ("X="+this.x+" Y="+this.y);
     }
 }
