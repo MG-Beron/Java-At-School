476a010b07fe95dc0fd5e1d4c286e7f3a94913e6
Dako Dimov <dako98@gmail.com>
2016-10-25 18:23:49 +0300

    Update Rectangle.java

--- rectangle/Rectangle.java
+++ rectangle/Rectangle.java
@@ -1,30 +1,47 @@
 package rectangle;
 
 public class Rectangle {
+    
+        private Point p1;
+        private Point p2;
 
-    private Point a;
-    private Point b;
 
-    public Rectangle(Point a, Point b) {
-        this.a = a;
-        this.b = b;
+    public Rectangle(Point p1, Point p2){
+                    if(p1.getX()==p2.getX() || p1.getY()==p2.getY()){
+        throw new IllegalArgumentException("Identicle points.");
+            }
+        else{
+        this.p1=p1;
+        this.p2=p2;
+        }
     }
 
-    public Rectangle(int x1, int y1, int x2, int y2) {
-        this(new Point(x1, y1), new Point(x2, y2));
+    public Rectangle(int x1, int y1, int x2, int y2){
+
+        this(new Point(x1,y1), new Point(x2,y2));
     }
 
-    public double getPerimeter() {
-        double sideA = Math.abs(this.a.getX() - this.b.getX());
-        double sideB = Math.abs(this.a.getY() - this.b.getY());
-
-        return 2 * (sideA + sideB);
+    public Rectangle(){
+        this(new Point(),new Point());
     }
+    
+    public double perimeter(){
+        double sideA = Math.abs(this.p1.getX()-this.p2.getX());
+        double sideB = Math.abs(this.p1.getY()-this.p2.getY());
+        return 2*(sideA+sideB);
+    }
+    
+    public double diagonal(){
+        return Math.sqrt(Math.abs(p1.getX()-p2.getX()+p1.getY()-p2.getY()));
+    }
+    
+    public double area(){
+        return Math.abs(p1.getX()-p2.getX()*p1.getY()-p2.getX());
+        }
 
-    public double getArea() {
-        double sideA = Math.abs(this.a.getX() - this.b.getX());
-        double sideB = Math.abs(this.a.getY() - this.b.getY());
-
-        return sideA * sideB;
+    @Override
+    public String toString(){
+        String message="Point 1: ("+p1.getX()+";"+p1.getY()+") Point 2: ("+p2.getX()+";"+p2.getY()+")";
+        return message;
     }
 }
