41bda617731c1f8a0879e692165af5caa14431d0
Dako Dimov <dako98@users.noreply.github.com>
2016-11-03 16:36:43 +0200

    Made Height and Width public.

--- graphics/Main.java
+++ graphics/Main.java
@@ -4,9 +4,12 @@ import javax.swing.*;
 
 public class Main {
 
+public static int width=840;
+public static int height=560;
+    
     public static void main(String[] args) {
         JFrame window = new JFrame();
-        window.setSize(840, 560);
+        window.setSize(width, height);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().add(new GraphicsDemo());
         window.setVisible(true);
